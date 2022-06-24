7bd7629bc19e194713fb0adb7e8acd67ae62e23b
Juan Camilo <zarkprogame@gmail.com>
2022-06-23 23:34:54 -0500

    Demo del Juego

--- HANGMAN.py
+++ HANGMAN.py
@@ -1,29 +1,82 @@
+from operator import contains
 import random
 import os
 
 os.system('cls')
 
-titulo = """
+title = """
 **   **    ***    **   **  *******   **      **    ***    **   **      *****   **  **      *******    ***    ******  **  **
 **   **   ** **   ***  **  **        ***    ***   ** **   ***  **      **   *  **  **          ***   ** **   **   *  ** **
 *******   *****   **** **  **  ***   ****  ****   *****   **** **      *****     **          **      *****   ******  ***
 **   **  **   **  ** ****  **   **   ** **** **  **   **  ** ****      **   *    **        ***      **   **  **  *   ** **
 **   **  **   **  **   **  *******   **  **  **  **   **  **   **      *****     **        *******  **   **  **   *  **  **
 """
+start = """
+   ===============|==============
+     ||           |         ||
+     ||           |         ||
+     ||                     ||
+     ||                     ||
+     ||                     ||
+     ||==========═══========||=
+     ||                     ||  ==
+     ||                     ||    == 
+     ||                     ||      ==                 
+___________________________________________        
+    o    o    o    o    o     o    o    
+   /|\  /|\  /|\  /|\  /|\   /|\  /|\ 
+   / \  / \  / \  / \  / \   / \  / \  
+"""
+won = """
+   ===============|==============
+     ||           |         ||              
+     ||           |         ||                  
+     ||   o   I WON!        ||                  
+     ||  /|┘                ||                      
+     ||  / \                ||                          
+     ||==========═══========||=
+     ||                     ||  ==
+     ||                     ||    == 
+     ||                     ||      ==                 
+___________________________________________     
+   
+    claps!  claps!  claps!  claps!                           
+    o    o    o    o    o     o    o    
+   /|┘  /|┘  /|┘  /|┘  /|┘   /|┘  /|┘ 
+   / \  / \  / \  / \  / \   / \  / \  
+"""
+lose = """
+   ===============|==============
+     ||           |         ||
+     ||           |         ||         
+     ||           |         ||                
+     ||           |         ||
+     ||           |         ||
+     ||======||   |   ======||=
+     ||      ||   o         ||  ==
+     ||          /|\        ||    == 
+     ||          / \        ||      ==    
+     ||                     ||        ==
+___________________________________________
+    You Lost!     You Lost!    You Lost!                           
+    o    o    o    o    o     o    o    
+   /|┘  /|┘  /|┘  /|┘  /|┘   /|┘  /|┘ 
+   / \  / \  / \  / \  / \   / \  / \  
+"""
+
 animals = './archivos/animals.txt'
 colors = './archivos/colors.txt'
-animes = './archivos/animes.txt'
-mangas = './archivos/mangas.txt'
+cities = './archivos/cities.txt'
 
-print(titulo)
+print(title)
+print(start)
 
 game = ("""
 -----GAMEMODES----
 
     1 for animals
     2 for colors
-    3 for animes
-    4 for mangas or manhwas
+    3 for cities
 """)
 
 print(game)
@@ -36,18 +89,15 @@ while True:
     elif gamemode == '2':
         choice = colors
     elif gamemode == '3':
-        choice = animes
-    elif gamemode == '4':
-        choice = mangas
+        choice = cities
     else:
         os.system('cls')
-        print(titulo)
+        print(title)
         print(game)
         print('Only have these options')     
         continue
     break
-###############  1. agregar pistas para animes y mangas
-###############  2. ponerle oportunidades, con dibujos ASCI en GitHub
+
 words = []
 with open(choice, 'r' , encoding='utf-8') as f:
     for line in f:
@@ -73,19 +123,37 @@ word = list(word)
 word.pop(-1)
 word = ''.join(word)
 
+counter = 10
+use_char = []
+
 while True:
+    cont_temp = 0
     os.system('cls')
-    print(titulo)
+    print(title)
+    print(start)
     print('Guess the Word: ')
     print(secret)
+    print('Used characters = ', use_char)
+    print('You have',counter, 'oportunities')
     if secret == new_word:
-        print('Ganaste, La Palabra era ', word)
+        os.system('cls')
+        print(title)
+        print(won)
+        print('You Won!!, The Word was', word)
         break
     user_word = input('Type a letter and press Enter: ')
     for index,char in enumerate(word_list):
         if user_word == char:
+            cont_temp = 1
             secret_list[index] = char
             secret = ''.join(secret_list)
-    print(secret)
-    
+    if cont_temp == 0:
+        counter -= 1
+        use_char.append(user_word)
+    if counter < 1:
+        os.system('cls')
+        print(title)
+        print(lose)
+        print('You didnt guess the Word, The Word was',word)
+        quit()
 
